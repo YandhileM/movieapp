1338f02c96ea64c1b7d449078cad1cd4479acf23
Yandhile <yandhilemaluleke@gmail.com>
2025-10-18 18:32:38 +0200

    feat: integrate updateSearchCount function to track movie search queries

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,8 +3,9 @@ import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
+import { updateSearchCount } from "@/services/appwrite";
 import useFetch from "@/services/useFetch";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 
 const Search = () => {
@@ -13,6 +14,8 @@ const Search = () => {
     data: movies,
     loading: loading,
     error: error,
+    refetch: loadMovies,
+    reset
   } = useFetch(
     () =>
       fetchMovies({
@@ -20,6 +23,20 @@ const Search = () => {
       }),
     false
   );
+  useEffect(() => {
+
+    const timeoutId = setTimeout(async () => {
+      if (searchQuery.trim()) {
+        await loadMovies();
+        if(movies?.length > 0 && movies?.[0]){
+          await updateSearchCount(searchQuery, movies[0])
+        }
+      } else {
+        reset();
+      }
+    })
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery]);
   return (
     <View className="flex-1 bg-primary">
       <Image
