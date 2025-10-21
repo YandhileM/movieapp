0433ffcd2eac21ac1d69cfb318304c8316626ba9
Yandhile <yandhilemaluleke@gmail.com>
2025-10-21 19:56:51 +0200

    feat: add fetchMovieDetails hook

--- services/api.tsx
+++ services/api.tsx
@@ -18,11 +18,30 @@ export const fetchMovies = async ({query} : {query: string}) => {
     });
 
     if(!response.ok){
+        //@ts-ignore
         throw new Error('Failed to fetch movies', response.statusText);
 
     }
 
     const data = await response.json();
     return data.results;
+
+}
+
+export const fetchMovieDetails = async (movieId: string) => {
+    try {
+        const response = await fetch(`${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+            {
+                method: 'GET',
+                headers: TMDB_CONFIG.Headers
+            }
+        )
+        if(!response.ok) throw new Error('Failed to fetch movie details')
+        
+        const data = await response.json()
+    } catch(error) {
+        console.log(error)
+        throw error
+    }
 
 }
